eb8b73857af17c8e19a3c013b33ef1ba4f78cae3
Wyatt Childers <wchilders@nearce.com>
2019-03-10 23:30:29 -0400

    Improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,31 +1,24 @@
 import React from "react"
-import { graphql } from "gatsby"
 
+import Bootstrap from "../components/bootstrap"
+import NavBar from "../components/nav-bar"
+import PrimaryContainer from "../components/primary-container"
 import SEO from "../components/seo"
 
 class NotFoundPage extends React.Component {
   render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-
     return (
       <div>
+        <Bootstrap />
+        <NavBar />
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <PrimaryContainer>
+          <h1>Oh no :(</h1>
+          <p>You found a page that no longer exists, or was never created.</p>
+        </PrimaryContainer>
       </div>
     )
   }
 }
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
