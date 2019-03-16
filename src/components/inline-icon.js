aab9decf80925998e66a771b0c13d0867605b740
Wyatt Childers <wchilders@nearce.com>
2019-03-15 22:07:04 -0400

    Improve about page layout on small screens

--- src/components/inline-icon.js
+++ src/components/inline-icon.js
@@ -8,7 +8,7 @@ config.autoAddCss = false
 class InlineIcon extends React.Component {
   render() {
     return (
-        <span style={{paddingRight: '.25em'}}>
+        <span style={{paddingRight: this.props.paddingAmt || '.25em'}}>
           <FontAwesomeIcon icon={ this.props.icon } style={{width: '1em', height: '1em', verticalAlign: '-0.125em'}} />
         </span>
     );
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,16 +2,23 @@ import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 
+import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 import Bootstrap from "../../components/bootstrap"
 import NavBar from "../../components/nav-bar"
 import PrimaryContainer from "../../components/primary-container"
 import SEO from "../../components/seo"
+import InlineIcon from "../../components/inline-icon"
 
 class AboutMe extends React.Component {
   render() {
     const { data } = this.props
     const content = data.content.edges[0].node
     const photo = data.portrait.childImageSharp
+
+    const github = `https://github.com/DarkArc/`
+    const twitter = `https://twitter.com/haxing_ninja`
+    const linkedin = `https://www.linkedin.com/in/wyattchilders/`
 
     return (
       <div>
@@ -23,21 +30,32 @@ class AboutMe extends React.Component {
         />
         <PrimaryContainer>
           <div className="row mt-3">
-            <div className="col-md-9" dangerouslySetInnerHTML={{ __html: content.html }} />
-            <div className="col-md-3">
-              <div className="d-none d-sm-block d-md-none pt-3"></div>
+            <div className="col-md-9">
+              <div className="d-sm-block d-md-none">
+                <div className="mx-auto" style={{maxWidth: '255px'}}>
+                  <Img className="rounded-circle d-block w-100" fluid={photo.fluid} />
+                  <div className="d-flex justify-content-around mt-3 mb-2" style={{fontSize: '2rem'}}>
+                    <a href={github}><InlineIcon icon={faGithub} paddingAmt="0px" /></a>
+                    <a href={twitter}><InlineIcon icon={faTwitter} paddingAmt="0px" /></a>
+                    <a href={linkedin}><InlineIcon icon={faLinkedin} paddingAmt="0px" /></a>
+                  </div>
+                </div>
+              </div>
+              <div dangerouslySetInnerHTML={{ __html: content.html }} />
+            </div>
+            <div className="col-md-3 d-none d-md-block">
               <div className="mx-auto" style={{maxWidth: '255px'}}>
                 <Img className="rounded-circle d-block w-100" fluid={photo.fluid} />
               </div>
               <ul className="nav flex-column text-center mt-3">
                 <li className="nav-item">
-                  <a className="nav-link" href="https://github.com/DarkArc/">GitHub</a>
+                  <a className="nav-link" href={github}>GitHub</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="https://twitter.com/haxing_ninja">Twitter</a>
+                  <a className="nav-link" href={twitter}>Twitter</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="https://www.linkedin.com/in/wyattchilders/">LinkedIn</a>
+                  <a className="nav-link" href={linkedin}>LinkedIn</a>
                 </li>
               </ul>
             </div>
